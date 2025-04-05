01f79a396d04fffc1eceac6269a80b398eaa9d46
DanielnSerpa <danielnserpa@live.com>
2025-04-05 22:16:46 +0100

    "Login-and-Signup-interface"

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -1,2 +1,113 @@
-package org.example;public class Login {
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+
+    JLabel labelLogInTitle, labelCardNumber, labelPin;
+    JTextField textInsertCardNumber;
+    JPasswordField passwordInsertPin;
+    JButton buttonSignIn, buttonClear, buttonSignUp;
+
+    Login() {
+
+        super("ATM System");
+
+        ImageIcon card1 = new ImageIcon(ClassLoader.getSystemResource("icon/card.png"));
+        Image card2 = card1.getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT);
+        ImageIcon card3 = new ImageIcon(card2);
+        JLabel cardimage = new JLabel(card3);
+        cardimage.setBounds(318, 10, 100, 100);
+        add(cardimage);
+
+        labelLogInTitle = new JLabel("WELCOME TO THE ATM SYSTEM");
+        labelLogInTitle.setForeground(Color.WHITE);
+        labelLogInTitle.setFont(new Font("AvantGarde", Font.BOLD, 23));
+        labelLogInTitle.setBounds(185, 125, 450, 40);
+        add(labelLogInTitle);
+
+        labelCardNumber = new JLabel("Card No: ");
+        labelCardNumber.setForeground(Color.WHITE);
+        labelCardNumber.setFont(new Font("Ralway", Font.BOLD, 18));
+        labelCardNumber.setBounds(185, 190, 375, 30);
+        add(labelCardNumber);
+
+        labelPin = new JLabel("PIN: ");
+        labelPin.setForeground(Color.WHITE);
+        labelPin.setFont(new Font("Ralway", Font.BOLD, 18));
+        labelPin.setBounds(185, 250, 375, 30);
+        add(labelPin);
+
+        textInsertCardNumber = new JTextField(15);
+        textInsertCardNumber.setBounds(318, 190, 230, 30);
+        textInsertCardNumber.setFont(new Font("Arial", Font.BOLD, 14));
+        add(textInsertCardNumber);
+
+        passwordInsertPin = new JPasswordField(15);
+        passwordInsertPin.setBounds(318, 250, 230, 30);
+        passwordInsertPin.setFont(new Font("Arial", Font.BOLD, 14));
+        add(passwordInsertPin);
+
+        buttonSignIn = new JButton("SIGN IN");
+        buttonSignIn.setForeground(Color.BLACK);
+        buttonSignIn.setBounds(250, 305, 100, 30);
+        buttonSignIn.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonSignIn.addActionListener(this);
+        add(buttonSignIn);
+
+        buttonClear = new JButton("CLEAR");
+        buttonClear.setForeground(Color.BLACK);
+        buttonClear.setBounds(380, 305, 100, 30);
+        buttonClear.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonClear.addActionListener(this);
+        add(buttonClear);
+
+        buttonSignUp = new JButton("SIGN UP");
+        buttonSignUp.setForeground(Color.BLACK);
+        buttonSignUp.setBounds(250, 355, 230, 30);
+        buttonSignUp.setFont(new Font("Arial", Font.BOLD, 14));
+        buttonSignUp.addActionListener(this);
+        add(buttonSignUp);
+
+        ImageIcon bank1 = new ImageIcon(ClassLoader.getSystemResource("icon/bankbg.jpg"));
+        Image bank2 = bank1.getImage().getScaledInstance(736, 582, Image.SCALE_DEFAULT);
+        ImageIcon bank3 = new ImageIcon(bank2);
+        JLabel bankimage = new JLabel(bank3);
+        bankimage.setBounds(0, 0, 736, 552);
+        add(bankimage);
+
+        setLayout(null);
+        setSize(736, 582);
+        setLocation(450, 200);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        try {
+            if (e.getSource() == buttonSignIn) {
+                // insert logic
+            } else if (e.getSource() == buttonClear) {
+                textInsertCardNumber.setText("");
+                passwordInsertPin.setText("");
+            } else if (e.getSource() == buttonSignUp) {
+                // insert logic
+            }
+
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        new Login();
+
+    }
 }
--- src/main/java/org/example/Signup.java
+++ src/main/java/org/example/Signup.java
@@ -1,2 +1,90 @@
-package org.example;public class Signup {
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import com.toedter.calendar.JDateChooser;
+
+public class Signup extends JFrame {
+
+    JLabel signUpTitle, personalDetails, labelFirstName, labelLastName, labelDOB, labelGender;
+    JTextField textFirstName, textLastName;
+    JDateChooser chooseDate;
+
+    Signup() {
+        super ("APPLICATION FORM");
+
+        ImageIcon card1 = new ImageIcon(ClassLoader.getSystemResource("icon/card.png"));
+        Image card2 = card1.getImage().getScaledInstance(125, 125, Image.SCALE_DEFAULT);
+        ImageIcon card3 = new ImageIcon(card2);
+        JLabel cardimage = new JLabel(card3);
+        cardimage.setBounds(25, 10, 100, 100);
+        add(cardimage);
+
+        Random random = new Random();
+        long randomDigits = Math.abs(random.nextLong() % 9000L + 1000L);
+
+        signUpTitle = new JLabel("APPLICATION FORM NO. " + randomDigits);
+        signUpTitle.setBounds(185, 20, 600, 40);
+        signUpTitle.setFont(new Font("Raleway", Font.BOLD, 38));
+        add(signUpTitle);
+
+        personalDetails = new JLabel("Personal Details");
+        personalDetails.setFont(new Font("Raleway", Font.BOLD, 22));
+        personalDetails.setBounds(330, 80, 600, 30);
+        add(personalDetails);
+
+        labelFirstName = new JLabel("First Name: ");
+        labelFirstName.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelFirstName.setBounds(185, 190, 150, 30);
+        add(labelFirstName);
+
+        textFirstName = new JTextField();
+        textFirstName.setFont(new Font("Raleway", Font.BOLD, 14));
+        textFirstName.setBounds(340, 190, 350, 30);
+        add(textFirstName);
+
+        labelLastName = new JLabel("Last Name: ");
+        labelLastName.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelLastName.setBounds(185, 240, 150, 30);
+        add(labelLastName);
+
+        textLastName = new JTextField();
+        textLastName.setFont(new Font("Raleway", Font.BOLD, 14));
+        textLastName.setBounds(340, 243, 350, 30);
+        add(textLastName);
+
+        labelDOB = new JLabel("Date of Birth: ");
+        labelDOB.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelDOB.setBounds(185, 290, 200, 30);
+        add(labelDOB);
+
+        chooseDate = new JDateChooser();
+        chooseDate.setForeground(new Color(105, 105, 105));
+        chooseDate.setBounds(340, 290,370, 30);
+        add(chooseDate);
+
+        labelGender = new JLabel("Gender: ");
+        labelGender.setFont(new Font("Raleway", Font.BOLD, 20));
+        labelGender.setBounds(185, 340, 200, 30);
+        add(labelGender);
+
+
+
+
+
+
+        getContentPane().setBackground(new Color(222, 255, 228));
+        setLayout(null);
+        setSize(850, 800);
+        setLocation(360, 40);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        new Signup();
+
+    }
 }
