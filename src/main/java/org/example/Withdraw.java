c3b783836873898a7e0e04dd093392c67ca6c987
DanielnSerpa <danielnserpa@live.com>
2025-04-19 09:21:31 +0100

    "ADDED Withdraw fast buttons"

--- src/main/java/org/example/Withdraw.java
+++ src/main/java/org/example/Withdraw.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 public class Withdraw extends JFrame implements ActionListener {
 
-    JLabel labelWithdrawAmount;
+    JLabel labelWithdrawFastCash, labelWithdrawAmount;
     JTextField textWithdrawAmount;
     JButton buttonSubmitWithdraw, buttonCancelWithdraw, button50, button100, button200, button500, button1000, buttonOther;
     String cardNo;
@@ -27,30 +27,74 @@ public class Withdraw extends JFrame implements ActionListener {
         atmImage.setBounds(0, 0,1260, 850);
         add(atmImage);
 
-        labelWithdrawAmount = new JLabel("SELECT WITHDRAW AMOUNT: ");
-//        labelWithdrawAmount = new JLabel("ENTER THE AMOUNT YOU WISH TO WITHDRAW: ");
-        labelWithdrawAmount.setForeground(Color.white);
-        labelWithdrawAmount.setFont(new Font("System", Font.BOLD, 18));
-        labelWithdrawAmount.setBounds(500, 335, 450, 35);
-        atmImage.add(labelWithdrawAmount);
+        labelWithdrawFastCash = new JLabel("SELECT WITHDRAW AMOUNT: ");
+        labelWithdrawFastCash.setForeground(Color.white);
+        labelWithdrawFastCash.setFont(new Font("System", Font.BOLD, 18));
+        labelWithdrawFastCash.setBounds(500, 335, 450, 35);
+        atmImage.add(labelWithdrawFastCash);
 
+
+        // Tambem adicionar depois que o usuario clicar em other amount
+//        labelWithdrawAmount = new JLabel("ENTER THE AMOUNT YOU WISH TO WITHDRAW: ");
+//        labelWithdrawAmount.setForeground(Color.white);
+//        labelWithdrawAmount.setFont(new Font("System", Font.BOLD, 18));
+//        labelWithdrawAmount.setBounds(425, 335, 450, 35);
+//        atmImage.add(labelWithdrawAmount);
+
+        // Adicionar depois que o usuario clicar em Other amount
         textWithdrawAmount = new JTextField();
         textWithdrawAmount.setBounds(425, 385, 405, 25);
         textWithdrawAmount.setFont(new Font("Raleway", Font.PLAIN, 20));
         textWithdrawAmount.setHorizontalAlignment(JTextField.CENTER);
-        atmImage.add(textWithdrawAmount);
+//        atmImage.add(textWithdrawAmount);
+
+        button50 = new JButton("€50");
+        button50.setBounds(400, 408, 75, 27);
+        button50.setForeground(Color.BLACK);
+        button50.addActionListener(this);
+        atmImage.add(button50);
+
+        button100 = new JButton("€100");
+        button100.setBounds(400, 445, 75, 27);
+        button100.setForeground((Color.BLACK));
+        button100.addActionListener(this);
+        atmImage.add(button100);
+
+        button200 = new JButton("€200");
+        button200.setBounds(400, 482, 75, 27);
+        button200.setForeground((Color.BLACK));
+        button200.addActionListener(this);
+        atmImage.add(button200);
+
+        button500 = new JButton("€500");
+        button500.setBounds(790, 408, 75, 27);
+        button500.setForeground((Color.BLACK));
+        button500.addActionListener(this);
+        atmImage.add(button500);
+
+        button1000 = new JButton("€1000");
+        button1000.setBounds(790, 445, 75, 27);
+        button1000.setForeground((Color.BLACK));
+        button1000.addActionListener(this);
+        atmImage.add(button1000);
+
+        buttonOther = new JButton("Other");
+        buttonOther.setBounds(790, 482, 75, 27);
+        buttonOther.setForeground((Color.BLACK));
+        buttonOther.addActionListener(this);
+        atmImage.add(buttonOther);
 
         buttonSubmitWithdraw = new JButton("CONFIRM");
         buttonSubmitWithdraw.setBounds(645, 470, 100, 35);
         buttonSubmitWithdraw.setForeground(Color.BLACK);
         buttonSubmitWithdraw.addActionListener(this);
-        atmImage.add(buttonSubmitWithdraw);
+//        atmImage.add(buttonSubmitWithdraw);
 
         buttonCancelWithdraw = new JButton("CANCEL");
         buttonCancelWithdraw.setBounds(500, 470, 100, 35);
         buttonCancelWithdraw.setForeground(Color.BLACK);
         buttonCancelWithdraw.addActionListener(this);
-        atmImage.add(buttonCancelWithdraw);
+//        atmImage.add(buttonCancelWithdraw);
 
         setLayout(null);
         setSize(1260, 850);
